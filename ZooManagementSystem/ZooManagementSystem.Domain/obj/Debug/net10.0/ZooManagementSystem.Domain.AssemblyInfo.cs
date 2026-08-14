edef0ce12451b2356a7705dac9742ffe35346e23
iktushar01 <iktushar2020@gmail.com>
2026-08-14 11:30:24 +0600

    fix: Update assembly informational versions and cache files for ZooManagementSystem

--- ZooManagementSystem/ZooManagementSystem.Domain/obj/Debug/net10.0/ZooManagementSystem.Domain.AssemblyInfo.cs
+++ ZooManagementSystem/ZooManagementSystem.Domain/obj/Debug/net10.0/ZooManagementSystem.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooManagementSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e6171152cbdbb747fc70834fa3e017ec20ddfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474a2e20fbf3703d5d3326654d1f301953a08969")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooManagementSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooManagementSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
